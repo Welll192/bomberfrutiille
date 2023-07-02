617598516f555dabd86a407c3d6a7cbd1dc9df25
ArmandoLluen <92061855+ArmandoLluen@users.noreply.github.com>
2023-07-02 18:29:31 -0500

    deteccion puño cerrado

--- Controller.py
+++ Controller.py
@@ -1,5 +1,6 @@
 import cv2
 import mediapipe as mp
+import numpy as np
 from View import *
 
 os.environ['SDL_VIDEO_CENTERED'] = '1'  # centrar pantalla
@@ -12,6 +13,28 @@ class Controller:
         self.video = cv2.VideoCapture(0)
         self.mp_drawing = mp.solutions.drawing_utils
         self.mano = mp.solutions.hands
+
+    # Función para detectar si un puño está cerrado
+    def detectar_puno(self, mano):
+        puntos = np.array([[p.x, p.y, p.z] for p in mano.landmark])
+        dedo_pulgar = puntos[4] - puntos[2]
+        dedo_indice = puntos[8] - puntos[6]
+        dedo_medio = puntos[12] - puntos[10]
+        dedo_anular = puntos[16] - puntos[14]
+        dedo_menique = puntos[20] - puntos[18]
+
+        if (dedo_pulgar[2] > 0 and dedo_indice[2] > 0 and dedo_medio[2] > 0 and dedo_anular[2] > 0 and dedo_menique[
+            2] > 0):
+            dedo_pulgar_angulo = np.arctan2(dedo_pulgar[1], dedo_pulgar[0])
+            dedo_indice_angulo = np.arctan2(dedo_indice[1], dedo_indice[0])
+            dedo_medio_angulo = np.arctan2(dedo_medio[1], dedo_medio[0])
+            dedo_anular_angulo = np.arctan2(dedo_anular[1], dedo_anular[0])
+            dedo_menique_angulo = np.arctan2(dedo_menique[1], dedo_menique[0])
+
+            if dedo_pulgar_angulo > dedo_indice_angulo > dedo_medio_angulo > dedo_anular_angulo > dedo_menique_angulo:
+                return True
+
+        return False
 
 
     def update(self):
@@ -58,20 +81,22 @@ class Controller:
 
                     fingers_vertical_pos = (thumb_tip.y + index_finger_tip.y + middle_finger_tip.y +
                                             ring_finger_tip.y + pinky_tip.y) / 5
+                    fingers_horizontal_pos = (thumb_tip.x + index_finger_tip.x + middle_finger_tip.x +
+                                            ring_finger_tip.x + pinky_tip.x) / 5
 
-                    if pygame.event.get(pygame.KEYDOWN) and pygame.key.get_pressed()[pygame.K_a]:
+                    if all(tip.y > pinky_tip.y for tip in [thumb_tip, index_finger_tip, middle_finger_tip, ring_finger_tip]):
                         cv2.putText(image, 'Mano cerrada', (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
                         self.V.model.hero.putBomb(self.V.model, time)
                     elif fingers_vertical_pos < thumb_tip.y or (pygame.event.get(pygame.KEYDOWN) and pygame.key.get_pressed()[pygame.K_UP]):
                         cv2.putText(image, 'Dedos arriba o boton arriba', (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
                         self.V.model.hero.move(self.V.model, 0, 1)
-                    elif fingers_vertical_pos > pinky_tip.y or (pygame.event.get(pygame.KEYDOWN) and pygqame.key.get_pressed()[pygame.K_DOWN]):
+                    elif fingers_vertical_pos > pinky_tip.y or (pygame.event.get(pygame.KEYDOWN) and pygame.key.get_pressed()[pygame.K_DOWN]):
                         cv2.putText(image, 'Dedos abajo o boton abajo', (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
                         self.V.model.hero.move(self.V.model, 0, -1)
-                    elif fingers_vertical_pos < thumb_tip.x or (pygame.event.get(pygame.KEYDOWN) and pygame.key.get_pressed()[pygame.K_LEFT]):
+                    elif fingers_horizontal_pos < thumb_tip.x or (pygame.event.get(pygame.KEYDOWN) and pygame.key.get_pressed()[pygame.K_LEFT]):
                         cv2.putText(image, 'Dedos a la derecha o boton derecha', (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0),2)
                         self.V.model.hero.move(self.V.model, 1, 0)
-                    elif fingers_vertical_pos > pinky_tip.x or (pygame.event.get(pygame.KEYDOWN) and pygame.key.get_pressed()[pygame.K_RIGHT]):
+                    elif fingers_horizontal_pos > pinky_tip.x or (pygame.event.get(pygame.KEYDOWN) and pygame.key.get_pressed()[pygame.K_RIGHT]):
                         cv2.putText(image, 'Dedos a la izquierda o boton izquierda', (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0),2)
                         self.V.model.hero.move(self.V.model, -1, 0)
 
